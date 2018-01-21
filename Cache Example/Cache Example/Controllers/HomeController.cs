5c3db1bfe3510f5187a0f3a4618758ad6371bb78
Mesut KAYA <mesutkaya@anadolu.edu.tr>
2018-01-21 17:41:18 +0300

    Simple Cache Example

---
+++ Cache Example/Cache Example/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cache_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [OutputCache (Duration = 60)]
+        public ActionResult ShowTime()
+        {
+            ViewBag.Time =  DateTime.Now;
+            return View();
+        }
+
+    }
+}
---
+++ Cache Example/Cache Example/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cache_Example.MvcApplication" Language="C#" %>
